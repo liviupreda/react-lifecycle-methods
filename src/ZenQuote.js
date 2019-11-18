c943b3c5ba3fdadf8eb18b202d9d5918d9468ea6
Liviu Preda <liviupreda@gmail.com>
2019-11-18 10:23:02 +0200

    npm i axios; add ZenQuote; load data from GitHub Zen API; setState with data and display random quote

---
+++ src/ZenQuote.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class ZenQuote extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { quote: "" };
+  }
+
+  componentDidMount() {
+    axios.get("https://api.github.com/zen").then(res => {
+      this.setState({ quote: res.data });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Always remember ... </h1>
+        <p>{this.state.quote}</p>
+      </div>
+    );
+  }
+}
+
+export default ZenQuote;
